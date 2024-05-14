aa037012a1fd47b80bd8bd0e86f2f4d426c777c0
WilderDuarte <waduarten@ufpso.edu.co>
2024-05-14 14:37:44 -0500

    Metodos y array

--- js/funtcions.js
+++ js/funtcions.js
@@ -49,6 +49,7 @@ function load_page(){
         imageAlt: "A tall image",
         showConfirmButton: false
     });
+    document.getElementById("valores").value = array_ejm; 
 }
 
 function send_info(){
@@ -71,4 +72,72 @@ function send_info(){
     }
 }
 
+//ARRAYS
+var dias_sem = ["Lunes","Martes","Miércoles","Jueves","Viernes","Sábado","Domingo"];
+var array_num= [1,2,3,4,5,6,7,8,9];
+var array_mix= [1,2.5,"abc"];
+var array_mul= [
+    {name:"Wilder",age:32,color:"green"},
+    {name:"Anyi",age:33,color:"white"},
+    {name:"Celeste",age:3,color:"black"},
+    {name:"Antonella",age:2,color:"gray"}
+]
 
+var json_ejm = {
+    name:"Wilder",
+    lastname:"Duarte",
+    phone:"3168667751",
+    email:"waduarten@ufpso.edu.co"
+}
+
+console.log(dias_sem);
+console.log(array_num);
+console.log(array_mix);
+console.log(array_mul);
+console.log(json_ejm);
+
+for(let i=0;i<dias_sem.length;i++){
+    console.log(dias_sem[i]);
+}
+
+var j=0;
+while(j<array_num.length){
+    console.log(array_num[j]);
+    j++
+}
+
+var acum = 0;
+for(let h=0;h<array_num.length;h++){
+    acum += array_num[h]
+    // acum = acum + array_num[h]
+}
+console.log(acum);
+
+var suma_age = 0;
+for(let a=0;a<array_mul.length;a++){
+    suma_age += array_mul[a].age;
+}
+console.log(suma_age);
+document.getElementById("print_age").innerText = "El valor de la suma es: "+suma_age;
+document.getElementById("print_age_eti").innerHTML = "<strong>El valor de la suma es: "+suma_age+"</strong>";
+
+function limpiar() {
+    document.getElementById("valores").value = "";
+    document.getElementById("print_age").innerText = "";
+    document.getElementById("print_age_eti").innerHTML = "";
+}
+
+var array_ejm = [1,2,3,4,5,6,7,8,9];
+
+function agregar(){
+    array_ejm.push(10);
+    document.getElementById("valores").value = array_ejm; 
+}
+function eliminar(){
+    array_ejm.pop();
+    document.getElementById("valores").value = array_ejm; 
+}
+function imp_rever(){
+    document.getElementById("text_rever").innerText = array_ejm.reverse();
+    document.getElementById("valores").disabled = false;
+}
